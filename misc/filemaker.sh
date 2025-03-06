b4ccdf0d6de13d59592c3762e46dfcea8e8381a7
Callin Ceriani <150146491+CallinCeriani@users.noreply.github.com>
2025-03-06 18:03:04 +0200

    Delete misc/filemaker.sh

--- misc/filemaker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-function make_files() {
-for file in *.fasta; do
-  dir_name="${file%.*}" # Remove the file extension to get the directory name
-  mkdir -p "$dir_name" # Create the directory
-  mv "$file" "$dir_name/" # Move the file into the directory
-done
-    }
