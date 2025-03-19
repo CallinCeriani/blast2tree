ef5404a5df3d5a4bcec2565137f35d2a6ba60c78
Callin Ceriani <150146491+CallinCeriani@users.noreply.github.com>
2025-03-19 11:51:24 +0200

    Update tree.sh

--- lib/tree.sh
+++ lib/tree.sh
@@ -24,12 +24,16 @@ tree()
     }
     
     # Total number of analysis steps
-    total_steps=3
+    total_steps=4
     current_step=1
 
-   run_analysis "$blast2tree" "mafft alignment" "eval mafft --adjustdirectionaccurately --threadtb $Cpus --threadit $Cpus  --thread $Cpus --auto $CLEAN_FILE > $output_dir/$MARKER_NAME.aligned.fasta"
+   run_analysis "$mafft" "mafft alignment" "eval mafft --adjustdirectionaccurately --threadtb $Cpus --threadit $Cpus --thread $Cpus --auto $CLEAN_FILE > $output_dir/$MARKER_NAME.aligned.fasta"
    awk '/^>/ {print; next} {gsub("n", "N"); print}' $output_dir/$MARKER_NAME.aligned.fasta > $output_dir/temp && mv $output_dir/temp $output_dir/$MARKER_NAME.aligned.fasta
-   run_analysis "$blast2tree" "trimal" "trimal -in $output_dir/$MARKER_NAME.aligned.fasta -out $output_dir/$MARKER_NAME.trimmed.fasta -automated1"
-   run_analysis "$blast2tree" "iqtree" "iqtree2 -s $output_dir/$MARKER_NAME.trimmed.fasta -m MFP -bb 1000 -alrt 1000 -nt AUTO --threads-max $Cpus -redo"
+   #sed -i 's/n/N/g' $output_dir/$MARKER_NAME.aligned.fasta
+   #run_analysis "$muscle" "muscle alignment"  "muscle -in $CLEAN_FILE -out $output_dir/$MARKER_NAME.aligned.fasta"  
+   run_analysis "$trimal" "trimal" "trimal -keepheader -in $output_dir/$MARKER_NAME.aligned.fasta -out $output_dir/$MARKER_NAME.trimmed.fasta -automated1"
+   run_analysis "$iqtree" "iqtree" "iqtree2 -s $output_dir/$MARKER_NAME.trimmed.fasta -m MFP -bb 1000 -alrt 1000 -nt AUTO --threads-max $Cpus -redo"
+   #-b 1000 (serios) -B 1000 (not serios)
+   # Gene concordance
 }
 
